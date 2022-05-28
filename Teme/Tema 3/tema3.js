d496afdf6821fc18254b2d9a1c295f1bac3adfe4
Raluca P <ralucapurice05@gmail.com>
2022-05-28 12:44:47 +0300

    inserting text in html with js

--- Teme/Tema 3/tema3.js
+++ Teme/Tema 3/tema3.js
@@ -48,3 +48,35 @@ console.log(personName + ' has ' + amountOfMoney + moneyLocation + dot);
 console.log(amountOfMoney + aLot + dot);
 
 console.log('========================================================');
+
+
+
+
+let h1Element = document.getElementById('someText');
+console.log(h1Element);
+console.log(h1Element.innerHTML);
+
+
+h1Element.innerHTML = 'Some text edited by JS';
+h1Element.style.color = 'red';
+
+
+let supraTitleElement = document.createElement('h4');
+supraTitleElement.innerHTML = 'this is a supra-title';
+
+
+h1Element.parentNode.insertBefore(supraTitleElement, h1Element);
+
+
+let subtema1 = document.createElement('h1');
+subtema1.innerHTML = (premise + activity3 + ' the ' + activity1 + ' of ' + activity2 + '.');
+h1Element.parentNode.insertBefore(subtema1, h1Element);
+
+
+let subtema2 = document.createElement('p');
+subtema2.innerHTML = (personName + ' plans' + action + carName + dot);
+h1Element.parentNode.insertBefore(subtema2, h1Element);
+
+
+
+

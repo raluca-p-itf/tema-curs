a31f550f4b07939b1ca2132cad977058d890e17e
Raluca P <ralucapurice05@gmail.com>
2022-05-28 11:04:41 +0300

    Jane Smith & Toyota Yaris

--- Teme/Tema 3/tema3.js
+++ Teme/Tema 3/tema3.js
@@ -1,3 +1,5 @@
+// Temă obligatorie și simplă
+
 let premise = 'What a nice day for ';
 
 let activity1 = 'science';
@@ -7,3 +9,42 @@ let activity2 = 'running';
 let activity3 = 'learning';
 
 console.log(premise + activity3 + ' the ' + activity1 + ' of ' + activity2 + '.');
+
+console.log('========================================================');
+
+
+// Temă obligatorie care necesită creativitate
+
+let personName = 'Jane Smith';
+let amountOfMoney = '$12.450';
+let moneyLocation = ' in her bank account';
+let aLot = ' is a lot of money';
+let carName = ' a Toyota Yaris';
+let action = ' to buy';
+let dot = '.';
+
+
+
+console.log(personName + ' has ' + amountOfMoney + moneyLocation + dot);
+console.log(amountOfMoney + aLot + dot);
+console.log(personName + ' plans ' + action + carName + dot);
+console.log(personName + ' will use her ' + amountOfMoney + action + carName + dot);
+console.log(amountOfMoney + aLot + ' for' + carName + dot);
+
+console.log('========================================================');
+
+console.log(amountOfMoney + aLot + dot);
+console.log(amountOfMoney + aLot + ' for' + carName + dot);
+console.log(personName + ' plans' + action + carName + dot);
+console.log(personName + ' has ' + amountOfMoney + moneyLocation + dot);
+console.log(personName + ' will use her ' + amountOfMoney + action + carName + dot);
+
+console.log('========================================================');
+
+console.log(personName + ' plans' + action + carName + dot);
+console.log(amountOfMoney + aLot + ' for' + carName + dot);
+console.log(personName + ' will use her ' + amountOfMoney + action + carName + dot);
+console.log(personName + ' has ' + amountOfMoney + moneyLocation + dot);
+console.log(amountOfMoney + aLot + dot);
+
+console.log('========================================================');

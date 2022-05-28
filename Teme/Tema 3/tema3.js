7857a2b6766ef8e571bee40fa1f037e80a09f32b
Raluca P <ralucapurice05@gmail.com>
2022-05-28 10:00:42 +0300

    what a nice day for... with js

---
+++ Teme/Tema 3/tema3.js
@@ -0,0 +1,9 @@
+let premise = 'What a nice day for ';
+
+let activity1 = 'science';
+
+let activity2 = 'running';
+
+let activity3 = 'learning';
+
+console.log(premise + activity3 + ' the ' + activity1 + ' of ' + activity2 + '.');

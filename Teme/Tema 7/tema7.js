eb1defd65c4b38fa6e40b605aab9811edabc7607
Raluca P <ralucapurice05@gmail.com>
2022-06-26 12:09:10 +0300

    hw7, ex1 - event on square (changing position)

---
+++ Teme/Tema 7/tema7.js
@@ -0,0 +1,29 @@
+// Tema obligatorie si simpla :)
+
+// 1. Moving a square on click
+
+let squareButton = document.querySelector('#theSquare');
+
+
+// a. initial position of the square
+let initialSquare = function() {
+    let squareButton = document.querySelector('#theSquare');
+    squareButton.style.position = 'fixed';
+    squareButton.style.top = '50px';
+    squareButton.style.left = '50px';
+}
+
+initialSquare();
+
+
+// b. moving position of the square on click (event)
+let moveSquare = function() {
+    let squareButton = document.querySelector('#theSquare');
+    squareButton.style.position = 'fixed';
+    squareButton.style.bottom = '50px';
+    squareButton.style.right = '50px';
+    squareButton.style.removeProperty('top');
+    squareButton.style.removeProperty('left');
+}
+
+squareButton.addEventListener('click', moveSquare);

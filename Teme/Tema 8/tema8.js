81a68fe16662882cf0f9c8221983737ebb920db1
Raluca P <ralucapurice05@gmail.com>
2022-07-02 11:35:57 +0300

    bootstrap table & axios for getting crew

--- Teme/Tema 8/tema8.js
+++ Teme/Tema 8/tema8.js
@@ -3,7 +3,7 @@ let popupCloseButton = document.querySelector('.closeButton > span');
 
 let popupClose = function () {
     // hide the popup
-    let tablePopup = document.querySelector('.table');
+    let tablePopup = document.querySelector('.tableWindow');
     tablePopup.style.display = 'none';
 };
 
@@ -16,9 +16,22 @@ let showPopup = document.querySelector('.btn');
 
 let popupOpen = function () {
     // show the popup
-    let tablePopup = document.querySelector('.table');
+    let tablePopup = document.querySelector('.tableWindow');
     tablePopup.style.display = 'block';
 };
 
 
 showPopup.addEventListener('click', popupOpen);
+
+
+axios
+    .get('https://api.spacexdata.com/v4/crew')
+    .then(function (response) {
+        // handle success
+        console.log(response);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+;    

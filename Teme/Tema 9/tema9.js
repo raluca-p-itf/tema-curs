1559f395f1d1b38c9601d68bad8ee04986793d9b
Raluca P <ralucapurice05@gmail.com>
2022-07-21 09:08:48 +0300

    hw 9 fix

--- Teme/Tema 9/tema9.js
+++ Teme/Tema 9/tema9.js
@@ -10,16 +10,87 @@ let currentDate = moment();
 
 
 // function that calculates every 6 days USING WHILE
+// let getDaysBetweenDates = function() {
+
+//     let cloningDates = startDate.clone();
+
+//     while (cloningDates.isSameOrBefore(currentDate)) {
+//         dates.push(cloningDates.format('ddd, MMMM D, YYYY'));
+//         cloningDates.add(6, 'days');
+//     }
+
+//     return dates;
+// };
+
+
+// function that calculates every 6 days USING FOR
+// let getDaysBetweenDates = function() {
+
+//     let cloningDates = startDate.clone();
+
+//     for (;cloningDates.isSameOrBefore(currentDate);) {
+//         dates.push(cloningDates.format('ddd, MMMM D, YYYY'));
+//         cloningDates.add(6, 'days');
+//     }
+
+//     return dates;
+// };
+
+
+// build table
+// for (let i=0; i<dates.length; i++) {
+
+//     let tableBody = document.querySelector('.tableBody');
+
+//     let row = document.createElement('tr');
+
+//     let numberCell = document.createElement('th');
+//     numberCell.classList.add("numberCell");
+
+//     let dataCell = document.createElement('td');
+//     dataCell.classList.add("dataCell");
+
+//     row.appendChild(numberCell);
+//     row.appendChild(dataCell);
+
+//     numberCell.innerHTML = [i+1];
+//     dataCell.innerHTML = dates[i];
+
+//     tableBody.appendChild(row);
+// };
+
+
+// a single function where i used FOR for both getting every 6 days and for building the table
 let getDaysBetweenDates = function() {
 
     let cloningDates = startDate.clone();
 
-    while (cloningDates.isSameOrBefore(currentDate)) {
+    for (let i=0; cloningDates.isSameOrBefore(currentDate); i++) {
         dates.push(cloningDates.format('ddd, MMMM D, YYYY'));
         cloningDates.add(6, 'days');
+        
+        let tableBody = document.querySelector('.tableBody');
+
+        let row = document.createElement('tr');
+
+        let numberCell = document.createElement('th');
+        numberCell.classList.add("numberCell");
+
+        let dataCell = document.createElement('td');
+        dataCell.classList.add("dataCell");
+
+        row.appendChild(numberCell);
+        row.appendChild(dataCell);
+
+        numberCell.innerHTML = [i+1];
+        dataCell.innerHTML = dates[i];
+
+        tableBody.appendChild(row);
+
     }
 
     return dates;
+
 };
 
 
@@ -31,46 +102,7 @@ let addCurrentDate = function() {
 }
 
 
-// function that calculates every 6 days USING IF AND FOR
-// let getDaysBetweenDates = function() {
-
-//     let now = startDate.clone();
-
-//     if (now.isSameOrBefore(currentDate)) {
-//         for (let i=0; i<100; i++) {
-//             dates.push(now.format('ddd, MMMM D, YYYY'));
-//             now.add(6, 'days');
-//         }
-//     }
-
-//     return dates;
-// };
-
-
 // calling both functions and print the arrays to console
 let datesList = getDaysBetweenDates();
 addCurrentDate();
 console.log(datesList);
-
-
-// build table
-for (let i=0; i<dates.length; i++) {
-
-    let tableBody = document.querySelector('.tableBody');
-
-    let row = document.createElement('tr');
-
-    let numberCell = document.createElement('th');
-    numberCell.classList.add("numberCell");
-
-    let dataCell = document.createElement('td');
-    dataCell.classList.add("dataCell");
-
-    row.appendChild(numberCell);
-    row.appendChild(dataCell);
-
-    numberCell.innerHTML = [i+1];
-    dataCell.innerHTML = dates[i];
-
-    tableBody.appendChild(row);
-};

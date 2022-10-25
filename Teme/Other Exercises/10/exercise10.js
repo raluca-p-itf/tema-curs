6a15a652511a767c1a32da94784d10006e62bf42
Raluca P <ralucapurice05@gmail.com>
2022-10-25 21:04:15 +0300

    exercises

--- Teme/Other Exercises/10/exercise10.js
+++ Teme/Other Exercises/10/exercise10.js
@@ -1,42 +1,16 @@
 const swiper = new Swiper('.swiper', {
-    effect: 'cards',
-    perSlideOffset: {
-        number: 8
-    },
-    perSlideRotate: {
-        number: 2
-    },
-    rotate: {
-        boolean: true
-    },
-    slideShadows: {
-        boolean: true
-    },
-    transformE1: {
-        CSSSelector: null
-    },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
 
 
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+    dynamicBullets: {
+      boolean: true
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+
+  }
+
+});
 
 
 
--- Teme/Other Exercises/TattooPage/tattoopage.js
+++ Teme/Other Exercises/TattooPage/tattoopage.js
@@ -14,3 +14,55 @@ function initMap() {
 }
 
 window.initMap = initMap;
+
+
+// Swiper for the presentation page
+
+const swiperPresentation = new Swiper('.swiperPresentation', {
+    effect: 'cards',
+    perSlideOffset: {
+        number: 8
+    },
+    perSlideRotate: {
+        number: 2
+    },
+    rotate: {
+        boolean: true
+    },
+    slideShadows: {
+        boolean: true
+    },
+    transformE1: {
+        CSSSelector: null
+    },
+
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
+
+
+// Swiper for the portofolios
+
+const swiper = new Swiper('.artistPortofolio', {
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+});

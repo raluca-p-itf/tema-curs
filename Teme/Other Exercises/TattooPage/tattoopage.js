8d2a73d9dda70493bb91302290d1266cc3599ec6
Raluca P <ralucapurice05@gmail.com>
2022-10-30 11:30:15 +0200

    exercises

--- Teme/Other Exercises/TattooPage/tattoopage.js
+++ Teme/Other Exercises/TattooPage/tattoopage.js
@@ -65,3 +65,16 @@ const swiper = new Swiper('.artistPortofolio', {
         draggable: true,
     },
 });
+
+
+
+// Mobile Menu
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
